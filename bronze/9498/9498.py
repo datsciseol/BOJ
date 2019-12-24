511385f0498dfb224e0baf9d5da7af170dfc469e
datsciseol <dat.sci.seol@gmail.com>
2019-12-25 04:57:29 +0900

    Add files via upload

---
+++ bronze/9498/9498.py
@@ -0,0 +1,16 @@
+test_score = int(input())
+
+if 90 <= test_score <= 100:
+    print("A")
+
+elif 80 <= test_score < 90:
+    print("B")
+
+elif 70 <= test_score < 80:
+    print("C")
+
+elif 60 <= test_score < 70:
+    print("D")
+
+else:
+    print("F")
